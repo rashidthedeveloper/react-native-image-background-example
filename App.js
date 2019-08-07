87984cb02af35c6ee4afb184988214edb85a6d84
Rashid <rashid@nextgbl.com>
2019-08-07 12:09:12 +0530

    second commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Text, StyleSheet, ImageBackground } from 'react-native';
+import React, { Component } from "react";
+import { Text, StyleSheet, ImageBackground } from "react-native";
 
 export default class App extends Component {
   constructor(props) {
@@ -12,10 +12,9 @@ export default class App extends Component {
       <ImageBackground
         source={{
           uri:
-            'https://cdn.pixabay.com/photo/2019/07/16/11/14/banff-4341560_960_720.jpg',
+            "https://cdn.pixabay.com/photo/2019/07/16/11/14/banff-4341560_960_720.jpg"
         }}
         style={styles.container}
-        imageStyle={styles.image}
       >
         <Text style={styles.header}>Hello World!</Text>
       </ImageBackground>
@@ -24,16 +23,13 @@ export default class App extends Component {
 }
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: "center",
-  },
-  image: {
-    opacity: 50
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: 'center'
   },
   header: {
     fontSize: 48,
-    color: 'white'
-  }
+    color: "white",
+  },
 });
